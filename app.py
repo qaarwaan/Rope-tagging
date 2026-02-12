d92579eac6f167863789c0690bf448a8efcec160
qaarwaan <qaarwaan@gmail.com>
2026-02-12 13:38:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,50 @@
 from flask import Flask, jsonify
-import json
+import psycopg2
+import os
 
 app = Flask(__name__)
 
-def load_ropes():
-    with open("ropes.json", "r") as file:
-        return json.load(file)
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+def get_connection():
+    return psycopg2.connect(DATABASE_URL)
 
 @app.route("/")
 def home():
-    return "Rope Tagging System is Live"
+    return "Rope Tagging System is Live (DB Connected)"
 
 @app.route("/rope/<rope_id>")
 def rope(rope_id):
-    ropes = load_ropes()
-    rope_data = ropes.get(rope_id)
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
 
-    if rope_data:
-        return jsonify({
-            "rope_id": rope_id,
-            "details": rope_data
-        })
-    else:
-        return jsonify({
-            "error": "Rope not found"
-        }), 404
+        cur.execute(
+    "SELECT rope_id, color, length, thickness, batch FROM rope_details WHERE rope_id = %s",
+    (rope_id,)
+)
+
+        row = cur.fetchone()
+
+        cur.close()
+        conn.close()
+
+        if row:
+            return jsonify({
+                "rope_id": row[0],
+                "details": {
+                    "color": row[1],
+                    "length": row[2],
+                    "thickness": row[3],
+                    "batch": row[4]
+                }
+            })
+        else:
+            return jsonify({"error": "Rope not found"}), 404
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == "__main__":
     app.run()
