e84108683f93eef8fe04370637fb1d338eb30f22
qaarwaan <qaarwaan@gmail.com>
2026-02-12 12:57:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, world!"
+    return "Rope Tagging System is Live"
+
+@app.route("/rope/<rope_id>")
+def rope(rope_id):
+    return f"Details for Rope ID: {rope_id}"
 
 if __name__ == "__main__":
     app.run()
